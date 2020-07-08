bccb5d05b4bea957c89629bef5df221ea1e4c74a
bamcasa <combeesang@gmail.com>
2020-07-09 02:55:44 +0900

    종이 차례대로 표시

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import numpy as np
+import time
 import pprint
 
 pygame.init()
@@ -11,8 +12,10 @@ class hole_game:
         self.FPS = 30
         self.screen_size = (500,850)
         self.square_size = 3
+        self.square_count = 3
+        self.square_number = 0
         self.screen = pygame.display.set_mode(self.screen_size)
-        self.square = np.zeros((self.square_size,self.square_size))
+        self.square = np.zeros((self.square_count,self.square_size,self.square_size))
         pygame.display.set_caption("Hole game")
     def show_background(self):
         pygame.draw.rect(self.screen, (243, 216, 141),(0,0,self.screen_size[0],self.screen_size[1])) #모든 화면 노란색으로 채우기
@@ -21,16 +24,19 @@ class hole_game:
         pygame.draw.rect(self.screen, (255, 255, 255), [50, 50, 400, 400], 5)  #테두리
         for i in range(self.square_size):
             for j in range(self.square_size):
-                if self.square[i][j]:
+                if self.square[self.square_number][j][i]:
                     pygame.draw.circle(self.screen, (255,255,255), [i * 150 + 100, j * 150 + 100], 40)
-                    pygame.draw.circle(self.screen, (190,190,190), [i * 150 + 100, j * 150 + 100], 40,2)
+                    pygame.draw.circle(self.screen, (190,190,190), [i * 150 + 100, j * 150 + 100], 40, 2)
 
     def make_square(self):
-        for i in range(self.square_size):
-            for j in range(self.square_size):
-                self.square[i][j] = random.randrange(0,2)
+        for k in range(self.square_count):
+            for i in range(self.square_size):
+                for j in range(self.square_size):
+                    self.square[k][j][i] = random.randrange(0,2)
+
     def main(self):
         clock = pygame.time.Clock()
+        self.make_square()
         while True:
             clock.tick(self.FPS)
             for event in pygame.event.get():
@@ -38,9 +44,12 @@ class hole_game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    self.make_square()
+                    self.square_number += 1
+                    if(self.square_number >= self.square_count):
+                        self.square_number = 0
             self.show_background()
             self.show_square()
+
             pygame.display.flip()
 
 if __name__ == "__main__":
